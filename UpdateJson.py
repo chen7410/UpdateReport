5d316204493f7482e90506acb0fedadc0ae349cd
Minqing <chen7410@uw.edu>
2019-08-13 11:30:23 -0700

    first commit

---
+++ UpdateJson.py
@@ -0,0 +1,36 @@
+import json
+def main():
+    jsonArray = []
+
+    for i in range(1, 11):
+        temp = {'id': i, 'image': str(i)+'.png', 'textPath': str(i)+'.txt', 'textPathModfied': '2019-9-9 12:12:12',
+    'summaryPath': str(i)+'summary.txt', 'summaryPathModfied': '2019-9-9 12:12:20', 'processTimeSecond': i+2}
+        jsonArray.append(temp)
+
+    for i in range(3, 11):
+        temp = {'id': i, 'image': str(i)+'.png', 'textPath': str(i)+'.txt', 'textPathModfied': '2019-9-9 12:12:12',
+    'summaryPath': str(i)+'summary.txt', 'summaryPathModfied': '2019-9-9 12:12:20', 'processTimeSecond': i+1}
+        jsonArray.append(temp)
+
+    firstJson = json.dumps(jsonArray)
+
+    # with open('./jsonfile.json', 'w') as outfile:
+    #     file = json.dump(jsonArray, outfile, indent=4)
+    # print(firstJson)
+
+    try:
+        data = json.loads(firstJson)
+        print ("Is valid json? true") 
+    except ValueError as e: 
+        print ("Is valid json? false") 
+
+    with open('jsonfile.json', 'r') as infile:
+        jsonObj = json.load(infile)
+        jsonObj.append({'id': 99, 'image': str(99)+'.png', 'textPath': str(99)+'.txt', 'textPathModfied': '2019-9-9 12:12:12',
+    'summaryPath': str(99)+'summary.txt', 'summaryPathModfied': '2019-9-9 12:12:20', 'processTimeSecond': 99})
+    print(jsonObj[-1]['id'])
+
+    with open('jsonfileAdded.json', 'w') as out:
+        newFile = json.dump(jsonObj, out, indent=4)
+
+main()
---
+++ UpdateReport.py
@@ -0,0 +1,38 @@
+import json
+import urllib.parse
+import boto3
+print('Loading function')
+
+s3 = boto3.client('s3')
+report_key = 'report/jsonfile.json'
+report_bucket = 'quicksightdata753'
+new_report_key = 'report/jsonfileAdded.json'
+
+reggie_bucket = 'result.store'
+reggie_key = 'testing.txt'
+
+
+def lambda_handler(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+
+    # Get the object from the event and show its content type
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    
+    try:
+        content = s3.get_object(Bucket=report_bucket, Key=report_key)
+        # print(content)
+        body = content['Body'].read().decode('utf-8')
+        json_array = json.loads(body)
+        json_array.append({'id': 99, 'image': str(99)+'.png', 'textPath': str(99)+'.txt', 'textPathModfied': '2019-9-9 12:12:12',
+        'summaryPath': str(99)+'summary.txt', 'summaryPathModfied': '2019-9-9 12:12:20', 'processTimeSecond': 99})
+        print(json_array[-1]['id'])
+        
+        json_body = json.dumps(json_array)
+        s3.put_object(Body=json_body, Bucket=report_bucket, Key=new_report_key)
+
+        return event
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
