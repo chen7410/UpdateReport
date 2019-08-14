1f04195772e634b6353ad736ef3c2a74d500645f
Minqing <chen7410@uw.edu>
2019-08-14 10:10:51 -0700

    delete try catch block

--- UpdateReport.py
+++ UpdateReport.py
@@ -8,10 +8,10 @@ print('Loading function')
 s3 = boto3.client('s3')
 report_key = 'matt/report/usage.json'
 report_key_gz = 'matt/reportgz/usage.json.gz'
-image_folder ='imgstore/'
+image_folder = 'imgstore/'
 text_folder = 'imgstore/'
 
-#buckts must in the same region as event bucket
+# buckts must in the same region as event bucket
 text_bucket = 'textracttext'
 report_bucket = 'textractreport'
 image_bucket = 'textractimage'
@@ -19,7 +19,8 @@ image_bucket = 'textractimage'
 
 def lambda_handler(event, context):
     summary_bucket = event['Records'][0]['s3']['bucket']['name']
-    summary_key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    summary_key = urllib.parse.unquote_plus(
+        event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     summary_region = event['Records'][0]['awsRegion']
     summary_event_time = event['Records'][0]['eventTime']
 
@@ -34,71 +35,66 @@ def lambda_handler(event, context):
     try:
         summary_content = s3.get_object(Bucket=summary_bucket, Key=summary_key)
         # print(summary_content)
-        image_content = s3.get_object(Bucket=image_bucket, Key=image_folder + image_name)
+        image_content = s3.get_object(
+            Bucket=image_bucket, Key=image_folder + image_name)
         # print(image_content)
         report_content = s3.get_object(Bucket=report_bucket, Key=report_key)
         # print(report_content)
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they '
-        + 'exist and your bucket is in the same region as this function.')
+              + 'exist and your bucket is in the same region as this function.')
         raise e
 
-    try:
-        summary_last_modified = summary_content['LastModified']
-        # print(summary_last_modified)
-        image_last_modified = image_content['LastModified']
-        # print(image_last_modified)
-        process_time = get_process_time(image_last_modified, summary_last_modified)
-        # print(process_time.seconds)
-        body = report_content['Body'].read().decode('utf-8')
-        # print(len(body))
-        if len(body) is 0:
-            print('len(body) is 0')
-            json_array = []
-            item = {
-                'id': 1,
-                'image_name': image_name,
-                'text_path': text_path,
-                'summary_path': summary_path,
-                'process_time': process_time.seconds
-            }
-            json_array.append(item)
-            json_body = json.dumps(json_array)
-            print(json_body)
-        else:
-            print('len(body) > 0')
 
-            item = {
-                'id': 1,
-                'image_name': image_name,
-                'text_path': text_path,
-                'summary_path': summary_path,
-                'process_time': process_time.seconds
-            }
-    except Exception as e:
-        print(e)
-        raise e
-        #TODO: insert data to json and save to s3
+        
+    summary_last_modified = summary_content['LastModified']
+    # print(summary_last_modified)
+    image_last_modified = image_content['LastModified']
+    # print(image_last_modified)
+    process_time = get_process_time(
+        image_last_modified, summary_last_modified)
+    # print(process_time.seconds)
+    body = report_content['Body'].read().decode('utf-8')
 
-        # json_array = json.loads(body)
-        # if len(json_array)== 0:
-        #     print('len is 0')
-        # else:
-        #     print('len > 0')
-        
-        # print(json_array[-1]['id'])
-        
-        # json_body = json.dumps(json_array)
-        # s3.put_object(Body=json_body, Bucket=report_bucket, Key=new_report_key)
-        
-    return event
+    json_array = []
+    if len(body) is 0:
+        # print('len(body) is 0')
+        item = {
+            'id': 1,
+            'image_name': image_name,
+            'text_path': text_path,
+            'summary_path': summary_path,
+            'process_time': process_time.seconds
+        }
+        json_array.append(item)
+
+    else:
+        # print('len(body) > 0')
+        json_array = json.loads(body)
+        item = {
+            'id': int(json_array[-1]['id']) + 1,
+            'image_name': image_name,
+            'text_path': text_path,
+            'summary_path': summary_path,
+            'process_time': process_time.seconds
+        }
+        json_array.append(item)
+ 
+    json_body = json.dumps(json_array)
+    s3.put_object(Body=json_body, Bucket=report_bucket, Key=report_key)
+    # print(json_body)
+
+    return 'Done'
+
 
 def get_process_time(start, end):
     return end - start
-    
+
+
 def get_path(bucket, key, region):
-        return 'https://' + bucket + '.s3-' + region + '.amazonaws.com/' + key
+    return 'https://' + bucket + '.s3-' + region + '.amazonaws.com/' + key
+
 
 def get_image_filename(bucket, image_folder, filename):
     s3res = boto3.resource('s3')
